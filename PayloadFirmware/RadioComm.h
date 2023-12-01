e35838f399ca752fea623f153b16b2e19a4ba068
Elijah Karl <ekarl@rockets.utoledo.edu>
2023-12-01 17:23:34 -0500

    added data structure for transmission

--- PayloadFirmware/RadioComm.h
+++ PayloadFirmware/RadioComm.h
@@ -1,3 +1,26 @@
 #ifndef RADIOCOMM_H
 
+#include <stdint.h>
+
+struct vec3 {
+  double x;
+  double y;
+  double z;
+};
+
+struct Packet {
+  long ID;
+  vec3 orientation;
+  vec3 accel;
+  vec3 vel;
+  long upTime;
+  long flightTime;
+  uint8_t state;
+};
+
+Packet makePacket(vec3 orient, vec3 accel, vec3 vel, uint8_t state);
+bool TxPacket(Packet p);
+
+
+
 #endif
