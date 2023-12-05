59ae324ec4b4c717edf9355823f026c8bd985771
Elijah Karl <ekarl@rockets.utoledo.edu>
2023-12-05 15:59:15 -0500

    packet construction code
    added packet construction thru addressing and casting

--- PayloadFirmware/RadioComm.cpp
+++ PayloadFirmware/RadioComm.cpp
@@ -1,2 +1,73 @@
 #include "RadioComm.h"
 
+RadioController::RadioController()
+{
+  Serial.begin(SERIAL_BAUD);
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  radio.setHighPower(); //must include this only for RFM69HW/HCW!
+  radio.encrypt(ENCRYPTKEY);
+}
+
+Packet RadioController::MakePacket(vec3 orient, vec3 accel, vec3 vel, uint8_t state)
+{
+  long upTime = millis();
+  long flightTime = upTime - launchTime;
+  
+  return Packet{
+    ID,
+    orient,
+    accel,
+    vel,
+    upTime,
+    flightTime,
+    state
+  };
+}
+
+bool RadioController::TxPacket(Packet p)
+{
+  char buff[48];
+  char frame0[8]; // ID
+  char frame1[48];// Orientation and Acceleraton
+  char frame2[41];// velocity, uptime, flight time, state
+
+  int i;
+  // frame 0 construction
+  for(i = 0; i < 8; i++)
+  {
+    frame0[i] = *(reinterpret_cast<unsigned char*>(&p.ID));
+  }
+
+  // frame 1 construction
+  for(i = 0; i < 24; i++)
+  {
+    frame1[i] = *(reinterpret_cast<unsigned char*>(&p.orientation));
+  }
+  for(i = 0; i < 24; i++)
+  {
+    frame1[i + 24] = *(reinterpret_cast<unsigned char*>(&p.accel));
+  }
+
+  // frame 2 construction
+  for(i = 0; i < 24; i++)
+  {
+    frame2[i] = *(reinterpret_cast<unsigned char*>(&p.vel));
+  }
+
+  for(i = 0; i < 8; i++)
+  {
+    frame2[i + 24] = *(reinterpret_cast<unsigned char*>(&p.upTime));
+  }
+
+  for(i = 0; i < 8; i++)
+  {
+    frame2[i + 32] = *(reinterpret_cast<unsigned char*>(&p.flightTime));
+  }
+
+  frame2[41] = *(reinterpret_cast<unsigned char*>(&p.state));
+
+  // transmission
+  sprintf(buff, frame0, 915);
+  sprintf(buff, frame1, 915);
+  sprintf(buff, frame2, 915);
+}
--- PayloadFirmware/RadioComm.h
+++ PayloadFirmware/RadioComm.h
@@ -6,6 +6,13 @@
 #include <SPIFlash.h>      //get it here: https://www.github.com/lowpowerlab/spiflash
 #include <SPI.h>           //included with Arduino IDE install (www.arduino.cc)
 
+#define SERIAL_BAUD 115200
+#define FREQUENCY   RF69_915MHZ
+#define NODEID      99
+#define NETWORKID   100
+#define GATEWAYID   1
+#define ENCRYPTKEY  "UTolRocketryBlue"
+
 struct vec3 {
   double x;
   double y;
@@ -25,8 +32,12 @@ struct Packet {
 class RadioController
 {
   private:
-    const uint8_t Frequency = RF69_915MHZ;
-
+    long launchTime;
+    const long ID = 0xFEFEFEF;
+    RFM69_ATC radio;
+    byte sendSize=0;
+    bool requestACK = false;
+    int TRANSMITPERIOD = 300; //transmit a packet to gateway so often (in ms)
   public:
     RadioController();
     Packet MakePacket(vec3 orient, vec3 accel, vec3 vel, uint8_t state);
